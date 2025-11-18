6963149852437099067c446d74008e9709e7a4b0
Filippo Menis <filippomeniswork@gmail.com>
2025-11-18 17:54:09 +0100

    chore(types) add @types/pg

--- scripts/applyMigrations.ts
+++ scripts/applyMigrations.ts
@@ -5,7 +5,7 @@ import { join, resolve } from "path";
 async function applyMigrations(): Promise<void> {
   const client = new pg.Client({
     host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
+    port: +process.env.PG_PORT!,
     database: process.env.PG_DB,
     user: process.env.PG_USER,
     password: process.env.PG_PW,
