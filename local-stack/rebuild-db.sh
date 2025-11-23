433c7a91e5325c4088017df8ef5cc80fedae4e70
Filippo Menis <filippomeniswork@gmail.com>
2025-11-23 11:41:12 +0100

    chore: enhance local env

---
+++ local-stack/rebuild-db.sh
@@ -0,0 +1,29 @@
+# /bin/bash
+
+echo "==============================="
+echo " Rebuild local stack"
+echo "==============================="
+
+cd ./local-stack
+
+echo "Stop postgres container"
+docker container stop postgres-dev
+echo "Stop redis container"
+docker container stop redis-dev
+echo "Stop nginx container"
+docker container stop nginx-dev
+
+echo "Remove postgres container"
+docker container rm postgres-dev
+echo "Remove redis container"
+docker container rm redis-dev
+echo "Remove nginx container"
+docker container rm nginx-dev
+
+echo "Remove postgres-dev data (volume)"
+docker volume rm postgres-dev
+echo "Remove redis-dev data (volume)"
+docker volume rm redis-dev
+
+echo "Relaunch stack"
+docker compose up -d
